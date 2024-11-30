dabf9ca4be9f856cd19cf0123a96a65d186d80fb
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2024-11-30 20:49:32 +0200

    Add logging and CLI argument parsing; configure log4rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, World!");
+use std::path::PathBuf;
+
+use clap::Parser;
+use log::{info, warn};
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long, default_value = "config")]
+    config: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    log4rs::init_file("log4rs.yaml", Default::default())?;
+    let _cli = Cli::parse();
+
+    info!("Cli Options: {_cli:#?}");
+    info!("test");
+    warn!("test");
+    Ok(())
 }
