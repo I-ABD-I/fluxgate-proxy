3734c8ee5200889ed1b99df82329dd3f2cbf1f28
Aylon Ben Dvora <70889286+I-ABD-I@users.noreply.github.com>
2025-03-17 21:07:22 +0200

    Update dependencies and enhance logging functionality; add ReadAndLog struct for improved data handling

--- src/logger.rs
+++ src/logger.rs
@@ -1,40 +1,59 @@
+use chrono::Local;
 use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
 use log4rs::append::file::FileAppender;
+use log4rs::config::runtime::{ConfigBuilder, LoggerBuilder};
 use log4rs::config::{Appender, Logger, Root};
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::Config;
+use std::mem;
 
-pub(super) fn create_logger(verbose: bool) -> anyhow::Result<()> {
+fn file_logger(builders: &mut [LoggerBuilder], config: &mut ConfigBuilder) -> anyhow::Result<()> {
+    for i in 0..builders.len() {
+        builders[i] = mem::take(&mut builders[i]).appender("file");
+    }
+
+    let now = Local::now();
+    let file = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new(
+            "{d(%Y-%m-%d %H:%M:%S)} {h({l})} [{h({t})}] {m}{n}",
+        )))
+        .build(format!("logs/{}.log", now.format("%Y-%m-%d_%H:%M:%S")))?;
+
+    *config = mem::take(config).appender(Appender::builder().build("file", Box::new(file)));
+    Ok(())
+}
+
+pub(super) fn create_logger(verbose: bool, log_to_file: bool) -> anyhow::Result<()> {
+    let now = Local::now();
     let console = ConsoleAppender::builder()
         .encoder(Box::new(PatternEncoder::new(
             "{d(%Y-%m-%d %H:%M:%S)} {h({l})} [{h({t})}] {m}{n}",
         )))
         .build();
 
-    let file = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{d(%Y-%m-%d %H:%M:%S)} {h({l})} [{h({t})}] {m}{n}",
-        )))
-        .build("logs/fluxgate.log")?;
-
-    let fluxgate_logger = Logger::builder()
-        .appenders(["console", "file"])
-        .build("fluxgate", LevelFilter::Debug);
-
     let tls_logger_level = if verbose {
         LevelFilter::Debug
     } else {
         LevelFilter::Info
     };
+    let fluxgate_logger = Logger::builder().appender("console");
 
-    let tls_logger = Logger::builder()
-        .appenders(["console", "file"])
-        .build("tls", tls_logger_level);
+    let tls_logger = Logger::builder().appender("console");
 
-    let config = Config::builder()
+    let mut config = Config::builder();
+
+    let mut loggers = [fluxgate_logger, tls_logger];
+
+    if log_to_file {
+        file_logger(&mut loggers, &mut config)?;
+    }
+
+    let fluxgate_logger = mem::take(&mut loggers[0]).build("fluxgate", LevelFilter::Debug);
+    let tls_logger = mem::take(&mut loggers[1]).build("tls", tls_logger_level);
+
+    let config = config
         .appender(Appender::builder().build("console", Box::new(console)))
-        .appender(Appender::builder().build("file", Box::new(file)))
         .loggers([fluxgate_logger, tls_logger])
         .build(Root::builder().build(LevelFilter::Off))?;
 
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ mod logger;
 
 use crate::logger::create_logger;
 use clap::Parser;
-use log::{debug, info};
+use log::{debug, error, info};
 use std::io::Write;
-use std::{fs, io::Read, net::TcpListener, path::PathBuf};
 use std::sync::Arc;
+use std::{fs, io::Read, net::TcpListener, path::PathBuf};
 use tls::config::ServerConfig;
-use tls::pki_types::{CertificateDer, PrivateKeyDer};
 use tls::pki_types::pem::PemObject;
-use tls::server::Connection;
+use tls::pki_types::{CertificateDer, PrivateKeyDer};
+use tls::server::Acceptor;
+use tls::stream::StreamOwned;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -23,9 +24,31 @@ struct Cli {
     verbose: bool,
 }
 
+struct ReadAndLog<T: Read> {
+    inner: T,
+    buffer: Vec<u8>,
+}
+
+impl<T: Read> ReadAndLog<T> {
+    fn new(inner: T) -> Self {
+        Self {
+            inner,
+            buffer: Vec::with_capacity(4096),
+        }
+    }
+}
+
+impl<T: Read> Read for ReadAndLog<T> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let len = self.inner.read(buf)?;
+        self.buffer.extend_from_slice(&buf[..len]);
+        Ok(len)
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-    create_logger(cli.verbose)?;
+    create_logger(cli.verbose, false)?;
 
     // panics, cannot run without proper config
     let cfg = ron::from_str::<Vec<config::Server>>(&fs::read_to_string(&cli.config)?)?;
@@ -35,7 +58,6 @@ fn main() -> anyhow::Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:4000")?;
 
-
     let certs = CertificateDer::pem_file_iter("certificate.crt")?
         .map(|cert| cert.unwrap())
         .collect();
@@ -44,8 +66,39 @@ fn main() -> anyhow::Result<()> {
     let _cfg = ServerConfig::builder().with_single_certificate(certs, key)?;
     let _cfg = Arc::new(_cfg);
 
-    for socket in listener.incoming() {
-        let mut stream = tls::stream::StreamOwned::new(Connection::new(_cfg.clone()), socket?);
+    'accept_loop: for socket in listener.incoming() {
+        let mut socket = socket?;
+        let mut acceptor = Acceptor::default();
+
+        let mut transport = ReadAndLog::new(&mut socket);
+        let accepted = loop {
+            acceptor.read_tls(&mut transport)?;
+            match acceptor.accept() {
+                Ok(Some(accepted)) => break accepted,
+                Ok(None) => continue,
+                Err((e, mut alert)) => {
+                    alert.write_all(&mut socket)?;
+                    error!("Failed Accepting Connection: {:?}", e);
+                    continue 'accept_loop;
+                }
+            }
+        };
+
+        debug!("Transport Buffer {:?}", transport.buffer);
+        info!("sni is {:?}", accepted.sni());
+
+        let connection = match accepted.into_connection(_cfg.clone()) {
+            Ok(connection) => connection,
+            Err((e, mut alert)) => {
+                alert.write_all(&mut socket)?;
+                error!("Failed Creating Connection: {:?}", e);
+                continue;
+            }
+        };
+
+        let mut stream = StreamOwned::new(connection, socket);
+        // let mut stream = tls::stream::StreamOwned::new(Connection::new(_cfg.clone()), socket?);
+
         let mut buf = [0u8; 2048];
         let len = match stream.read(&mut buf) {
             Ok(len) => len,
@@ -53,7 +106,7 @@ fn main() -> anyhow::Result<()> {
         };
         info!("{}", String::from_utf8(buf[..len].to_vec())?);
 
-        let _ = stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 993\r\nConnection: close\r\n\r\n")?;
+        stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 993\r\nConnection: close\r\n\r\n")?;
 
         let len = stream.write(
             br#"<!DOCTYPE html>
@@ -96,7 +149,7 @@ fn main() -> anyhow::Result<()> {
         <p>Welcome to your test page.</p>
     </div>
 </body>
-</html>"#
+</html>"#,
         )?;
         debug!("{:?}", len);
     }
